fc1f80a035ab4273c94c0c1fc4dbeb26a99354b1
Your Name <admin@shalev.eu.org>
2024-10-15 19:53:11 +0100

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,32 +4,29 @@ import node from "@astrojs/node";
 import cloudflare from "@astrojs/cloudflare";
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
-
 import starlight from "@astrojs/starlight";
 
+// 
 // https://astro.build/config
-// export default defineConfig({
 //   site: 'https://example.com',
 //   integrations: [mdx(), sitemap(), react(), starlight({
 //     title: 'code creators',
 //   })],
-//   output: "server",
-//   // adapter: cloudflare(),
-//   adapter: node({
-//     mode: "standalone"
-//   })
+import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [ starlight({
+  integrations: [starlight({
     disable404Route: true,
     title: 'code creators',
     customCss: [
-      // Relative path to your custom CSS file
-      './src/styles/custom.css',
-    ],  },)],
-  output: "server",
+    // Relative path to your custom CSS file
+    './src/styles/custom.css']
+  })],
+  output: "server"
   // adapter: cloudflare(),
-  adapter: node({
-    mode: "standalone"
-  })
+  // adapter: node({
+  //   mode: "standalone"
+  // })
+  ,
+  adapter: vercel()
 });
