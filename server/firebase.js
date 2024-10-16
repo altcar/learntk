05604da15747b242f1f3b9799a9d7b23f0118094
Shalev <admin@shalev.eu.org>
2024-10-16 22:17:18 +0000

    firebase-api

--- server/firebase.js
+++ server/firebase.js
@@ -1,9 +1,10 @@
 import { initializeApp, applicationDefault, cert } from 'firebase-admin/app';
 import { getFirestore, Timestamp, FieldValue, Filter } from 'firebase-admin/firestore';
 // import serviceAccount from './learntk-firebase-adminsdk-m5nol-af3788623c.json';
+import {config} from "dotenv"; config()
 
 initializeApp({
-  credential: cert('./learntk-firebase-adminsdk-m5nol-af3788623c.json')
+  credential: cert(process.env.FIREBASE_ADMIN_SDK),
 });
 
 export const db = getFirestore();
