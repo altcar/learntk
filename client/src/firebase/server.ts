3c2a9c50853b2a0c45a57cf1b441790e0bc67dbb
Shalev <admin@shalev.eu.org>
2024-10-19 16:27:41 +0000

    secret expose

--- client/src/firebase/server.ts
+++ client/src/firebase/server.ts
@@ -1,3 +1,4 @@
+
 import type { ServiceAccount } from "firebase-admin";
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 
@@ -5,8 +6,8 @@ const activeApps = getApps();
 const serviceAccount = {
   type: "service_account",
   project_id: "learntk",
-  private_key_id: "c288c65dc51b09c707298d682b964d54fe9ada61",
-  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCorqQz1SwsI61O\nxUIznj3s/jegfl5ZLX5BCvDseSpgIwtbSSJ3mqIHFNfLP6WkA0ZURLiZYEy6hyDc\n0byhpChXYQNN7mVq2IYClczTX0M5ojSrEe1/vfbofS2fDWNh5mjwhZ5hdpaC/jvj\nWCjjBWCIIwbNMZ5OME+6LTOh7Ouzd06lNSsevV0CMeMouCSHliQWJGkJQqhy3dvf\n531DWZX71q8rVoc0Lp4KETcZ8E27O15tALuhl2c9wprkoK628fbP4tyEuzqnm1Ma\n11jdtqH7y4pRttcG2saLElyC+C7Egc9ZAFVsA+vtNQj4wt6/8qXTf9YF3zeyNucK\nWzn7gLTLAgMBAAECggEAAYlumcHlJqIzYVNyKhBuUpslIsznJPUOQIWnsnsO7a3n\nQkj1MiJfCPIU+ApkgtnvljOQFczaT7XzQ//HMhcFGEXo5uXEPi53cU0v5UC2o8iv\npfVb9ON944CS3bwCyuQNOOlgV+8ukBMrGx2oNbVMiI3LivgOQxROmnNWx2PMtY33\na6vMMUeB7s2JutN5Qwi3nSwsKTlEmIPTbfv99kYwLpmS/bAU7tqBOC+WKNxN8Fxo\naoJ4DEYKCWeo19e6OcdflqNwV2M3E4zB71HAeljh0S0cKsYfeUwsh7TQQaLGaSvm\nld0Q5TmS5+mOSQzElC4EBZrQiSznxyxQHCS/I7rm8QKBgQC/TGesx6ktX9gFXKR3\nXyoPDgKXA89usZtx3wwPmJefalqQSyE64mIKztEXtixWBtIans6r5b++GwpjHROA\n1YxUX84F9Q0PjDna2b21YwDC99d4pcL8110Sicd6pB6cpq/zC6IHofAkjCKxqxmn\nBgYEzQ8lL3ufKSBT7eVBMsJG2wKBgQDhvAFNAI8voTS5PbvrMW9G8Pxrgu33F+X1\nd7y194X1X3hEEFMLLc/NX1kDSX/MX8IRCQZkIlwCCYLnsKtgQS172KOd6dzfnv0x\nvJUo0Uo66X0TTsq3PAhYb1TCYUI/sWMB1QrNma+Rv/fNlwnM3TY+JjGxDl1bMTNZ\nrcdwf39U0QKBgCElwCE0R6BRJ0FmmCkHGGabat94DY2EwQauO4zISB2bdC3kltF5\nQLvWsmyFaJFPhUCPnAg1O1BZpycddq+wUSydnTlzCNxU1AGUEUdI7GzcPT3umybs\nunfn3+TkY1mJmJtovJ7N9KqgjbJzj3JdnNVgIuuymp0tpyhj5IGzpe5ZAoGBALTl\nO8mgZB6QOjckcL6GrB0Wq8Hgd6CnJyJyJOlSBajgK/mnd1UjDj8PZL7MnLyEFHog\nWF3QZxk+1AelvYByWue/cUbwDY+q0k6USJggODNDv3bEkpEKa8mq1iq7VCLEnYnv\nVyvyOA893nCD1Fe3AmwFoZa44zlcEUGRkJznn3uxAoGAK6y0O/DfuoPH+Yr0tL8f\n6mgxXphV/LWoeNYCvQy0bn1rBJ7zaKZBeioAY/NxqRadW1LH/t+tgNUnTMV+AUpQ\n+6GQh6uBXXiJJj8QdCFgVG6ASRnXwj86Q6SQwtaaTtSGke8RbcF1NTRw5UNZJYMm\ntYEHu6GtY8ofpMT3aGncc/M=\n-----END PRIVATE KEY-----\n",
+  private_key_id: "add8d40cdb4fd4e5c7de9a793c3f4f501ba7c666",
+  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC2KF70gRHj5USn\nX3GER4IZYAV3sHiinvbt8Q8vaGFMOcy0QqFGwpvzhcxypzM70SdJbb8Xzt/SAd4y\n8YfqTZrzxdTkqKbHMeh7D0ymBALJgnsoBPcdiZ27cdkJquF2oq9pkY+EEdQyVPGQ\n3asr+7bsMsQ6jsH6ZCWMvnE1wl8Q3gLljIJh2MuoElSmF44/5NNlxjLKIVkgR7Vs\n+cdTCA4t6tAXg47u1jOPXjpwjfGaB05+HWMLZTE1MWNJk9UanQVDdRKe7yzU6KmS\n8lyXSnyn08OB5h9dLCHd13xjMCshl0dNT2sBq9Dgxe5lZbw4V5bgKfRoUg2lg/Tq\nluUMyZXRAgMBAAECggEABWS/nwS9V16gAEaysUR+xSqTLt6SS9piHFyBYxl3nA6w\nEXxRkytK+9Rv38s0mKZSP/nYaSeOeATnI+ID8LuYhXRLn/OgcJB5Pj+POPEwde//\nnok7GexLt9OgpgkhwOYDRKBzO+ldO5pJRs1Rdq1u1qKQLd2xP608cWToLDvTncew\nSwlU97ydZj2hHg5gGZeksXSuznFGwETj4dOo/uRT34jJsAZsEc7nJTOyIonN1r5y\nHIX6izKZKnhTs33K5m2CVMd5gNlQbYjRn4FtYpvh+fHD8ruYvJi/cJYje2rCRqQC\n+A2oZ1tqOm7lnvpvsX5cnJTpJ23WYwGj3bpiF56BIQKBgQDboCzylAUCx43MzPuc\nQ5Ns7dsDfCJg0L+P8A81s4e/QSuMGghKctZkMvP6s4ATs+e/m7IbnMbLhRl6yLAa\nNfA/F4C3o00wqjEtICioBKrPGNK+1miKUaLqkm9f0NJZhf+RArp9l27pKDHug2A/\n0v181+Nx6z6UWXLSIwmoyJ+GqQKBgQDUU5rLn3Hg6AibJozJSoRq3/UtLWZOtyPd\nGT3WN5FbirSurQ8TpE5Ptb/B+7n6lhLH0GZBzVJtMtTvDsaF5Xp6A1jpJ7g+ab6F\n+FrnKT8ZCL0XVmSnBvyUUsi8yXqvUhKV5S5LrFsrrqrQvHs0xR8/1hM50bT5WciY\nE/geQMmW6QKBgQC+UrNA0Cb7fqUSWnycsS7b0c/e2D8OeTfYa2cy2U6E3dv4AEM5\nCQVjNYias90Etf3Z3Pne1eCsU7592dZKEH4OnrZr7TciOgp9mYXB/LFs4I0Y1cMo\nQpCVCKs9dEAlVmrfZmboGUgDA1Mp522MQbJr8sZmXT8Q3ecT1Us0mg8saQKBgEKF\n0yja0r4JVLDvegqS6zvk4Rb/c19UJ8tc3i/ghw0sPwG/PMsFnneA1Qx8xr8JB+dM\nls0k/B80XtxrP62xwRa7N78M77+2N7gXoLtuA9PfFgdazPw7xyTdbwEfCu4lA6es\n/z08SfA8sE3nz54v6GOCudFSr6W0CJIFJ6aq274JAoGBANn/kVX/dBR+AWuFyOIl\nMZZ3sZQDMgleJuDm+S61/x3scnYdoMGsQFPQ88R4+sphtHYn8HCaA48soi/KX4b6\nT3zBKoqilNzq9oLzqx6xRiU3/4562phofoLoD+J7mK2I5vpPDWuiMoqTpOtr0gwK\nPlfpqxQhZWgS3FXHXiMmts1d\n-----END PRIVATE KEY-----\n",
   client_email: "firebase-adminsdk-m5nol@learntk.iam.gserviceaccount.com",
   client_id: "106253111285242958033",
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
@@ -14,19 +15,8 @@ const serviceAccount = {
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-m5nol%40learntk.iam.gserviceaccount.com",
   universe_domain: "googleapis.com"
-}
+};
 
-
-const initApp = () => {
-  if (process.env.PROD) {
-    console.info('PROD env detected. Using default service account.')
-    // Use default config in firebase functions. Should be already injected in the server by Firebase.
-    return initializeApp()
-  }
-  console.info('Loading service account from env.')
-  return initializeApp({
-    credential: cert(serviceAccount as ServiceAccount)
-  })
-}
-
-export const app = activeApps.length === 0 ? initApp() : activeApps[0];
+export const app = activeApps.length === 0 ? initializeApp({
+  credential: cert(serviceAccount as ServiceAccount),
+}) : activeApps[0];
