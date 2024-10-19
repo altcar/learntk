90845a9ce20f213abf108e696e8eeb200c92c0b9
Shalev <admin@shalev.eu.org>
2024-10-19 15:50:25 +0000

    redirect

--- client/src/firebase/client.ts
+++ client/src/firebase/client.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAlNnuFqcthCkwZCaqKWXxnon0GBN_2Ogg",
@@ -12,4 +12,4 @@ const firebaseConfig = {
   };
 
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
